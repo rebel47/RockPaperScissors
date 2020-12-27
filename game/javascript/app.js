b65425c2d78c25fce738b2caa341ede7c621cd4c
Mohd Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2020-12-27 20:44:39 +0530

    Update app.js

--- game/javascript/app.js
+++ game/javascript/app.js
@@ -120,7 +120,7 @@ function end_game() {
     change1();
     change2();
     change3();
-    window.location.href = "game/end.html";
+    window.location.href = "/end.html";
 }
 
 function main() {
